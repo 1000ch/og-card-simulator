1543777700c80edf013bebd5ab3af59911bba63c
1000ch <shogo.sensui@gmail.com>
2015-11-20 02:51:29 +0900

    indent

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,25 @@
 "use strict";
 
 module.exports = (request, response) => {
-  response.send(`<!doctype html>
-                  <html>
-                    <head>
-                      <meta charset="utf-8">
-                      <title>og-card-simulator</title>
-                      <link rel="stylesheet" href="css/app.css">
-                    </head>
-                    <body>
-                      <h2>Show open graph card from URL</h2>
-                      <form>
-                        <textarea></textarea>
-                      </form>
-                      <h2>Card example</h2>
-                      <div class="post">
-                        <div class="content"></div>
-                        <div class="card-wrapper"></div>
-                      </div>
-                      <script src="js/app.js"></script>
-                    </body>
-                  </html>`);
+  let html = `<!doctype html>
+              <html>
+                <head>
+                  <meta charset="utf-8">
+                  <title>og-card-simulator</title>
+                  <link rel="stylesheet" href="css/app.css">
+                </head>
+                <body>
+                  <h2>Show open graph card from URL</h2>
+                  <div>
+                    <textarea></textarea>
+                  </div>
+                  <h2>Card example</h2>
+                  <div class="post">
+                    <div class="content"></div>
+                    <div class="card-wrapper"></div>
+                  </div>
+                  <script src="js/app.js"></script>
+                </body>
+              </html>`;
+  response.send(html);
 };
