e15508fff3a63542c6df7cd8c0ef35cf5e853df1
1000ch <shogo.sensui@gmail.com>
2016-05-24 09:43:32 +0900

    Bump rog and its plugins

--- routes/api/og.js
+++ routes/api/og.js
@@ -3,7 +3,11 @@
 const rog = require('rog');
 
 module.exports = (request, response) => {
-  rog(request.body.url)
-    .then(data => response.json(data))
+  rog(request.body.url, {}, {
+    url         : require('rog-plugin-url'),
+    title       : require('rog-plugin-title'),
+    image       : require('rog-plugin-image'),
+    description : require('rog-plugin-description')
+  }).then(data => response.json(data))
     .catch(error => response.json(error));
 };
